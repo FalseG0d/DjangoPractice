1a288d5f7402bbf47394610f86f43fd731f641c1
Apoorv Garg <u.garg.10@gmail.com>
2020-06-04 21:07:29 +0530

    Update settings.py

--- DarkLightWebsite/settings.py
+++ DarkLightWebsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'kc6j4vw3^lpxhwyc+trp5be3po!*wyy4$i7x@$zqu!e0w&p3mk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False 
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','darklightwebsite.herokuapp.com']
 
 
 # Application definition
