92ccfafacb3867f2eed907130779efe82a16590e
Apoorv Garg <u.garg.10@gmail.com>
2020-06-07 13:57:52 +0530

    Update settings.py

--- DarkLightWebsite/settings.py
+++ DarkLightWebsite/settings.py
@@ -81,12 +81,12 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-'''
+
 
 import dj_database_url
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
-'''
+
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
